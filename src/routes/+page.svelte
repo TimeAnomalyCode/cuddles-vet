6493a8d53bf52a04a075713d622d0fd729dec97d
tingxi01 <tingxi99@gmail.com>
2023-10-15 00:21:10 +0800

    homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,79 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class=section1>
+    <img src="/homepage1.png" alt="Dog and cat" class="image1">
+    <h1>
+        Cuddles 
+        <br>Veterinary 
+        <br>Clinic
+    </h1>
+    <p1>
+        Where Every Paw Finds Compassion
+    </p1>
+    <p2>
+        <img src="/tel.png" alt="Telephone icon" class="icon">
+        012-714 1538
+    
+    </p2>
+    <p3>
+        <img src="/operatingHours.png" alt="Telephone icon" class="icon">
+        9:00 a.m. - 10:00 p.m.
+    </p3>
+</div>
+<div class=section2>
+    <h1>OUR HISTORY</h1>
+    
+
+
+</div>
+<head>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter" />
+</head>
+<style>
+
+    .section1{
+        width:100%;
+        background-color: #D0C4BE4D;
+        text-align: left;
+        height: 450px;
+        
+    }
+
+    .image1{
+        float:right;
+        padding: 0px 60px 0;
+    
+    }
+
+    h1{
+        padding: 60px 60px 0;
+        font-size:70px;
+        font-family: 'Inter', sans-serif;
+        font-weight: 900;
+        text-shadow: 3px 0 #736D69;
+        color: #736D69;
+
+    }
+
+    p1{
+        padding: 5px 60px 0;
+        font-size:15px;
+        font-family: 'Inter', sans-serif;
+        color: #736D69;
+    }
+    p2{
+        padding: 10px 60px 0;
+        font-size:13px;
+        font-family: 'Inter', sans-serif;
+        color: #736D69;
+    }
+    p3{
+        padding: 5px 60px 0;
+        font-size:13px;
+        font-family: 'Inter', sans-serif;
+        color: #736D69;
+    }
+    .icon{
+        width:20px;
+        height:20px;
+    }
+
+</style>
--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -1,9 +1,45 @@
-<div class="row">
-	<div class="col">
-		<h1>Products Page</h1>
+<script>
+	let searchTerm = '';
+	let products = [
+	  { name: 'Product 1' },
+	  { name: 'Product 2' },
+	  { name: 'Product 3' },
+	];
+  </script>
+  
+  <div class="container">
+	<h1>Products </h1>
+  
+	<div class="search-bar">
+	  <input
+		type="text"
+		placeholder="Search for products"
+		bind:value={searchTerm}
+	  />
 	</div>
-</div>
-
-<svelte:head>
-	<title>Cuddles Vet - Products</title>
-</svelte:head>
+  
+	<div class="product-list">
+	  {#each products as product (product.name)}
+		<div class="product">{product.name}</div>
+	  {/each}
+	</div>
+  </div>
+  
+  <style>
+	.container {
+	  	max-width: 6000px;
+	  	margin: 0 auto;
+	  	padding: 20px;
+	}
+  
+	.search-bar {
+		max-width: 6000px;
+	  	margin-bottom: 20px;
+	}
+  
+	.product {
+		border: 1px solid #ccc;
+	  	padding: 10px;
+	  	margin: 10px 0;
+	}
+  </style>
