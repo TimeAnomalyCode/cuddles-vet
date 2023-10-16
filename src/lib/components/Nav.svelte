de85d5a2d7714b0a105c33f552e17b4d5a895904
TimeAnomalyCode <77450287+TimeAnomalyCode@users.noreply.github.com>
2023-10-16 09:43:04 +0800

    fix nav blink bug

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -2,8 +2,9 @@
 	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
 	import { logout } from '$lib/firebase/auth.client';
-	import authStore from '$lib/stores/auth.store';
 	import messagesStore from '$lib/stores/messages.store';
+
+	export let isLoggedIn;
 
 	let isOpen = false;
 
@@ -24,8 +25,14 @@
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
 	<div class="container-fluid">
-		<span style="font-size: 25px" >
-			<img src="Logo.png" alt="Logo" width="64" height="70" class="d-inline-block align-text-center">
+		<span style="font-size: 25px">
+			<img
+				src="Logo.png"
+				alt="Logo"
+				width="64"
+				height="70"
+				class="d-inline-block align-text-center"
+			/>
 			Cuddles Vet Clinic
 		</span>
 		<div class:show={isOpen} class="nav justify-content-center" id="navbarNav">
@@ -37,8 +44,9 @@
 				</li>
 				<li class="nav-item">
 					<a
-						class:active={$page.url.pathname === '/appointment'} class="nav-link" href="/appointment"
-						>Appointment</a
+						class:active={$page.url.pathname === '/appointment'}
+						class="nav-link"
+						href="/appointment">Appointment</a
 					>
 				</li>
 				<li class="nav-item">
@@ -55,7 +63,7 @@
 		</div>
 		<div class:show={isOpen} id="navbarNav">
 			<ul class="navbar-nav">
-				{#if $authStore.isLoggedIn}
+				{#if isLoggedIn}
 					<li class="nav-item">
 						<a class:active={$page.url.pathname === '/profile'} class="nav-link" href="/profile"
 							>Profile</a
@@ -70,13 +78,13 @@
 					<!-- Not Logged In -->
 					<li class="nav-item">
 						<a class:active={$page.url.pathname === '/login'} class="nav-link" href="/login">
-							<img src="Profile.png" alt="Login" width="36" height="36">
-						  </a>
+							<img src="Profile.png" alt="Login" width="36" height="36" />
+						</a>
 					</li>
 					<li class="nav-item">
 						<a class:active={$page.url.pathname === '/cart'} class="nav-link" href="/cart">
-							<img src="Cart.png" alt="Cart" width="36" height="36">
-						  </a>
+							<img src="Cart.png" alt="Cart" width="36" height="36" />
+						</a>
 					</li>
 				{/if}
 			</ul>
@@ -88,15 +96,15 @@
 	.nav-item > span {
 		cursor: pointer;
 	}
-	div{
+	div {
 		font-family: inter;
-		color: #5C5957;
+		color: #5c5957;
 		font-weight: bold;
 	}
-	div div ul li{
+	div div ul li {
 		float: left;
 		padding: 20px;
-		color: #5C5957;
+		color: #5c5957;
 		text-decoration: none;
 		font-size: large;
 	}
