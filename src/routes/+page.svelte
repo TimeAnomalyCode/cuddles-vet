dc4cb1aa089dbc79c1135c7be13f834d4ce5a02e
tingxi01 <tingxi99@gmail.com>
2023-10-17 15:06:46 +0800

    Homepage- OUR HISTORY

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,24 +15,27 @@
         <img src="tel.png" alt="Telephone icon" class="icon">
         012-714 1538
         <br>
+        
         <img src="operatingHours.png" alt="Telephone icon" class="icon">
         9:00 a.m. - 10:00 p.m.
     </h2>
 </div>
 <h4>OUR HISTORY</h4>
 <img src="homepage2.png" alt="Dog and cat with stethoscope 2"  class= "image2">
-<div class="content-container">     
+<div class="paragraph1">     
     <content1> 
                 Cuddles Veterinary Clinic's history is a 
         <br>heartwarming tale of dedication and compassion. 
         <br>Founded in [Year] by the visionary Dr. [Founder's 
         <br>Name], our journey began with a singular mission: 
         <br>to champion the well-being of pets in [Location].
-
+        <br>
+        <br>
     </content1>
-    <content2 class=indented-paragraph>
-        
-        <br>Through the years, Cuddles Veterinary 
+</div>
+<div class="paragraph2">
+    <content2>
+            Through the years, Cuddles Veterinary 
         <br>Clinic has earned its place as a trusted institution, 
         <br>deeply rooted in our love for animals and our 
         <br>commitment to delivering exceptional veterinary 
@@ -46,7 +49,8 @@
 <style>
 
     .section1{
-        width: 100%;
+        margin-left:-110px;
+        width: 1517.5px;
         background-color: #D0C4BE4D;
         height: 450px;
         
@@ -55,6 +59,7 @@
     .image1{
         float:right;
         padding: 0px 60px 0;
+        margin-right:90px;
     
     }
 
@@ -67,8 +72,9 @@
     }
 
     h1{
+        margin-left:120px;
         padding: 60px 60px 0;
-        font-size:70px;
+        font-size:72px;
         font-family: 'Inter', sans-serif;
         font-weight: 900;
         text-shadow: 3px 0 #736D69;
@@ -77,12 +83,14 @@
     }
 
     p1{
+        margin-left:120px;
         padding: 5px 60px 0;
         font-size:15px;
         font-family: 'Inter', sans-serif;
         color: #736D69;
     }
     h2{
+        margin-left:120px;
         padding: 10px 60px 0;
         font-size:13px;
         font-family: 'Inter', sans-serif;
@@ -92,11 +100,21 @@
         width:20px;
         height:20px;
     }
-    .content-container {
+    .paragraph1 {
         float: right; /* Float the content container to the right */
         width: 50%; /* Adjust the width as needed */
-        margin-top:-290px;
-        font-size:17px;
+        margin-top:-310px;
+        font-size:15px;
+        color: #736D69B3;
+        padding-left:90px;
+        text-indent:50px;
+
+    }
+    .paragraph2 {
+        float: right; /* Float the content container to the right */
+        width: 50%; /* Adjust the width as needed */
+        margin-top:-180px;
+        font-size:15px;
         color: #736D69B3;
         padding-left:90px;
         text-indent:50px;
@@ -112,8 +130,6 @@
         padding: 60px;
     }
 
-    .indented-paragraph{
-        text-indent: 50px;
-    }
+    
 
 </style>
