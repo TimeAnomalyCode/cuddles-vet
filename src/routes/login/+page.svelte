3ea0931997667adc579089bb6a6aebe844da1688
Sherlyn0410 <sherlynkuan01@gmail.com>
2023-10-26 23:34:58 +0800

    login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -33,30 +33,54 @@
 
 <div class="row">
 	<div class="col">
+		<img src="SignUpBg.png" alt="background" class="image1">
+		
+	</div>
+	<div class="container">
 		<h5>Sign in to</h5>
-		<h1>Cuddles Veterinary Clinic</h1>
+		<h2>Cuddles Veterinary Clinic</h2>
+		<LoginWithGoogle />
+		<p>or</p>
+		<AuthForm btnName="Login" on:submit={onLogin} />
+		
+		<div class="row">
+			<div class="col">
+				<a href="/forgot-password" class="btn btn-warning w-100">Forgot Password</a>
+			</div>
+		</div>
+		<div class="signup">
+			<p>
+				Don't have an account? <a href="/signup">Create one now</a>
+			</p>
+		</div>
 	</div>
 </div>
 
-<hr />
-<AuthForm btnName="Login" on:submit={onLogin} />
-<hr />
-<LoginWithGoogle />
-<hr />
-<div class="row">
-	<div class="col">
-		<a href="/forgot-password" class="btn btn-warning w-100">Forgot Password</a>
-	</div>
-</div>
-<div class="signup">
-	<p>
-		Don't have an account? <a href="/signup">Create one now</a>
-	</p>
-</div>
+
 
 <style>
-	h5, h1, p{
+	h5, h2, p{
 		font-family: inter;
 		text-align: center;
+		font-weight: bold;
+	}
+	.image1{
+  		/* Control the height of the image */
+  		width: 100%;
+
+  		/* Center and scale the image nicely */
+  		background-position: center;
+ 		background-repeat: no-repeat;
+  		background-size: cover;
+  		position: relative;
+	}
+
+	.container {
+		position: absolute;
+		left: 120px;
+		margin: 20px;
+		max-width: 35%;
+		padding: 20px;
+		background-color: rgba(255, 255, 255, 0.7);
 	}
 </style>
