0ebdbce6f3e7f111edc015873a7098f23206216b
tingxi01 <tingxi99@gmail.com>
2023-10-28 01:15:31 +0800

    forgot password css

--- src/routes/forgot-password/+page.svelte
+++ src/routes/forgot-password/+page.svelte
@@ -25,16 +25,82 @@
 	}
 </script>
 
-<div class="row">
-	<div class="col">
-		<h1>Forgot Password</h1>
-	</div>
-</div>
-
-{#if !hideForm}
-	<AuthForm on:submit={onForgotPassword} btnName="Forgot Password" forgotPassword={true} />
-{/if}
-
 <svelte:head>
 	<title>Cuddles Vet - Forgot Password</title>
 </svelte:head>
+
+<div class="row">
+	<div class="col">
+		<img src="SignUpBg.png" alt="background" class="image1">
+		
+	</div>
+	<div class="container">
+		<a href="/login">
+			<img src="cross.png" alt= "cross icon" class="icon1">
+		</a>
+		<h2>Reset Password</h2>
+		<h5>Enter your e-mail address below</h5>
+
+		{#if !hideForm}
+			<AuthForm on:submit={onForgotPassword} btnName="Forgot Password" forgotPassword={true} />
+		{/if}	
+
+		
+	</div>
+</div>
+
+
+<style>
+	h5, h2 {
+		font-family: inter;
+		text-align: center;
+		color:#736D69
+	}
+	h5{
+		font-size:16px;
+		font-weight: bold;
+		margin-bottom:50px;
+		
+	}
+
+	h2{
+		font-size:28px;
+		font-weight:900;
+		text-shadow: 1px 0 #736D69;
+	}
+	.image1{
+  		/* Control the height of the image */
+  		width: 100%;
+
+  		/* Center and scale the image nicely */
+  		background-position: center;
+ 		background-repeat: no-repeat;
+  		background-size: cover;
+  		position: relative;
+	}
+
+	.icon1{
+  		/* Control the height of the image */
+  		width: 20px;
+		float:right;
+		margin-top:-30px;
+		margin-right:-30px;
+
+  		
+	}
+
+	.container {
+		position: absolute;
+		left: 120px;
+		margin-left: 50px;
+		margin-top: 50px;
+		max-width: 29%;
+		padding: 40px;
+		background-color: rgba(255, 255, 255, 0.6);
+	}
+
+	
+	
+	
+</style>
+
