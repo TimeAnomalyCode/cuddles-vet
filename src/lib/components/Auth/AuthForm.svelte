8fa896f95da8130c964fc9d60cbbf6f97c1bfeaf
tingxi01 <tingxi99@gmail.com>
2023-10-28 00:20:27 +0800

    removed password and email labels

--- src/lib/components/Auth/AuthForm.svelte
+++ src/lib/components/Auth/AuthForm.svelte
@@ -5,12 +5,12 @@
 
 <form on:submit|preventDefault>
 	<div class="mb-3">
-		<label for="email" class="form-label">Email</label>
+		
 		<input type="email" name="email" class="form-control" id="email" placeholder="Email" required />
 	</div>
 	{#if !forgotPassword}
 		<div class="mb-3">
-			<label for="password" class="form-label">Password</label>
+			
 			<input
 				type="password"
 				name="password"
