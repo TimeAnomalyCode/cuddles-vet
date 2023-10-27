842ccf26216762fd8ffb96fdc4f5743d314cdd85
tingxi01 <tingxi99@gmail.com>
2023-10-28 00:31:09 +0800

    log in form + button css

--- src/lib/components/Auth/AuthForm.svelte
+++ src/lib/components/Auth/AuthForm.svelte
@@ -5,7 +5,6 @@
 
 <form on:submit|preventDefault>
 	<div class="mb-3">
-		
 		<input type="email" name="email" class="form-control" id="email" placeholder="Email" required />
 	</div>
 	{#if !forgotPassword}
@@ -20,5 +19,31 @@
 			/>
 		</div>
 	{/if}
-	<button type="submit" class="btn btn-success w-100">{btnName}</button>
+	<button type="submit" class="btn btn-success ">{btnName}</button>
 </form>
+
+<style>
+	.mb-3{
+		width:320px;
+		margin: 0 auto; /* Horizontally center the text boxes */
+		
+	}
+	.form-control {
+		height: 50px; /* Adjust the height to your preference */
+	}
+
+	.btn{
+		width:320px;
+		background-color: #736D69;
+    	color: #ffffff;
+		margin-top:10px;
+		margin-left:20px;
+    	padding: 15px 20px;
+    	border: none;
+    	border-radius: 10px;
+    	cursor: pointer;
+    	font-size: 13px;
+		font-family: inter;
+
+	}
+</style>
