09169109858e8ce9446fa5201804fec350fdf229
tingxi01 <tingxi99@gmail.com>
2023-10-28 00:54:36 +0800

    create account form css

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -38,16 +38,90 @@
 	}
 </script>
 
-<div class="row">
-	<div class="col">
-		<h1>Sign Up</h1>
-	</div>
-</div>
-<hr />
-<AuthForm on:submit={register} btnName="Sign Up!" />
-<hr />
-<LoginWithGoogle />
-
 <svelte:head>
 	<title>Cuddles Vet - Sign up</title>
 </svelte:head>
+
+<div class="row">
+	<div class="col">
+		<img src="SignUpBg.png" alt="background" class="image1">
+	</div>
+	<div class="container">
+		<h5>Sign up for</h5>
+		<h2>Cuddles Veterinary Clinic</h2>
+		<LoginWithGoogle />
+
+		<p>or</p>
+			<AuthForm on:submit={register} btnName="Create Account" />
+
+		<div class="signup">
+			<p>
+				Already have an account? <a href="/login">Log in</a>
+			</p>
+		</div>
+	</div>
+</div>
+
+
+
+
+
+<style>
+	h5, h2, p{
+		font-family: inter;
+		text-align: center;
+		color:#736D69
+	}
+	h5, p, .signup{
+		font-size:16px;
+		font-weight: bold;
+		
+	}
+
+	h2{
+		font-size:28px;
+		font-weight:900;
+		text-shadow: 1px 0 #736D69;
+	}
+	.image1{
+  		/* Control the height of the image */
+  		width: 100%;
+
+  		/* Center and scale the image nicely */
+  		background-position: center;
+ 		background-repeat: no-repeat;
+  		background-size: cover;
+  		position: relative;
+	}
+
+	.container {
+		position: absolute;
+		left: 120px;
+		margin-left: 50px;
+		margin-top: 50px;
+		max-width: 29%;
+		padding: 40px;
+		background-color: rgba(255, 255, 255, 0.6);
+	}
+
+	.btn{
+		width:320px;
+		background-color: #736D69;
+    	color: #ffffff;
+		margin-top:15px;
+    	padding: 15px 20px;
+    	border: none;
+    	border-radius: 10px;
+    	cursor: pointer;
+    	font-size: 13px;
+		font-family: inter;
+		margin-left:20px;
+
+	}
+
+	.signup{
+		margin-top:20px;
+	}
+
+	
+</style>
