32d737e0545f6ee6cb825dcc2b420f36ff52396f
tingxi01 <tingxi99@gmail.com>
2023-10-30 10:31:56 +0800

    auth form css

--- src/lib/components/Auth/AuthForm.svelte
+++ src/lib/components/Auth/AuthForm.svelte
@@ -25,7 +25,7 @@
 
 <style>
 	.mb-3{
-		width:320px;
+		width:89%;
 		margin: 0 auto; /* Horizontally center the text boxes */
 		
 	}
@@ -37,15 +37,18 @@
 		width:89%;
 		background-color: #736D69;
     	color: rgba(255, 255, 255, 0.8);
-		font-size: 13.5px;
-		margin-top:10px;
-		margin-left:20px;
-    	padding: 15px 20px;
+		font-size: 0.86vw;
+		margin-top:3.5%;
+		margin-left:5.3%;
+    	padding: 4.5% 4.5%;
     	border: none;
     	border-radius: 10px;
     	cursor: pointer;
 		font-family: inter;
 		margin-left:5.3%;
+
+		
+	
 
 	}
 </style>
