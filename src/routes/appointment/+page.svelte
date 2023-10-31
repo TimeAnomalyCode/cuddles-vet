188487a120ad3415a249fbe2b5d5bbb7bb57d880
Sherlyn0410 <sherlynkuan01@gmail.com>
2023-10-31 18:57:35 +0800

    appointment

--- src/routes/appointment/+page.svelte
+++ src/routes/appointment/+page.svelte
@@ -1,34 +1,38 @@
-<head>
-	<title>Cuddles Vet - Appointment</title>
-	<script>
-
-	</script>
-</head>
-<body>
-	<div>
-		<img src="Background.png" alt="background" class="image1">
-		<h5 class="top-center">Give your pet the best care. Book an appointment now!</h5>
-	</div>
-</body>
-
-<style>
-	h5{
-		margin-top: 20px;
-		font-family: inter;
-		font-weight: bolder;
-		text-align: center;
-		color: #5C5957;
-	}
-	.image1 {
-    	width: 100%;
-	}
-
-	/* Top left text */
-	.top-center {
-  		position: absolute;
-  		top: 20%;
-  		left: 50%;
-  		transform: translate(-50%, -50%);
-	}
-
-	</style>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Cuddles Vet - Appointment</title>
+		<style>
+			.title{
+				font-family: inter Black, sans-serif;
+				font-weight: bolder;
+				font-size: 1.7vw;
+				color: #5C5957;
+				position: absolute;
+				top: 22%;
+				left: 50%;
+				transform: translate(-50%, -50%);
+			}
+			.image1 {
+				width: 100%;
+				background-position: center;
+				 background-repeat: no-repeat;
+				  background-size: cover;
+				  position: relative;
+			}
+	
+		
+		</style>
+	
+	</head>
+	<body>
+		<div>
+			<img src="Background.png" alt="background" class="image1">
+			<p class="title">Give your pet the best care. Book an appointment now!</p>
+		</div>
+		<div>
+		</div>
+	</body>
+	
+	
+</html>
