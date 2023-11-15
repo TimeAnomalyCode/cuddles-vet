fc2aaebdd975760ce9a67bda50f8705933a092a5
TimeAnomalyCode <77450287+TimeAnomalyCode@users.noreply.github.com>
2023-11-15 08:04:18 +0800

    force user to setup profile

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,6 +1,6 @@
-import { auth } from "$lib/firebase/firebase.server";
+import { addProfile } from "$lib/firebase/database.server";
+import { auth, db } from "$lib/firebase/firebase.server";
 import { redirect } from "@sveltejs/kit";
-import { getAuth } from "firebase/auth";
 
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
@@ -26,6 +26,30 @@ export async function handle({ event, resolve }) {
     const user = event.locals?.user
     const url = event.url
 
+    // console.log("sent")
+    // console.log(user)
+
+    let has_set_profile = false
+    if (user) {
+        const profileRef = await db.collection('profiles').doc(user.id).get()
+
+        if (profileRef.exists) {
+            has_set_profile = profileRef.data()?.has_set_profile
+        }
+
+        // console.log("Inside profile")
+        // console.log(has_set_profile)
+        // console.log(profileRef.data())
+
+        if (!has_set_profile && url.pathname !== '/profile-setup') {
+            throw redirect(302, `/profile-setup`)
+        }
+
+        if (!user.emailVerified) {
+            throw redirect(302, `/email-appprove`)
+        }
+    }
+
     if (url.pathname !== '/') {
         if (!user && protectedRoutes.find(u => url.pathname.indexOf(u) > -1)) {
             throw redirect(302, `/login?redirect=${url.pathname}`)
@@ -37,6 +61,10 @@ export async function handle({ event, resolve }) {
     }
 
     const response = await resolve(event)
+
+    // console.log("recieved")
+    // console.log(user)
+
     return response
 }
 
@@ -48,8 +76,16 @@ async function getFirebaseUser(token) {
     const decodedToken = await auth.verifyIdToken(token, true)
     const user = await auth.getUser(decodedToken.uid)
 
+    // const profile = {
+    //     user_id: user.uid,
+    //     name: user.displayName,
+    //     phone_number: user.phoneNumber,
+    // }
+    // await addProfile(profile)
+
     return {
         id: user.uid,
-        email: user.email
+        email: user.email,
+        emailVerified: user.emailVerified
     }
 }
