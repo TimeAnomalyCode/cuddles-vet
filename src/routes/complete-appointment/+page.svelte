2a64adc9941f0537d59e386efb44d27799c266c3
TimeAnomalyCode <77450287+TimeAnomalyCode@users.noreply.github.com>
2023-11-20 16:18:09 +0800

    confirm appointment

---
+++ src/routes/complete-appointment/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { goto } from '$app/navigation';
+	import messagesStore from '$lib/stores/messages.store';
+	import { redirect } from '@sveltejs/kit';
+	/** @type {import('./$types').PageData} */
+	export let data;
+
+	function cancelAppointment() {
+		goto('/appointment');
+	}
+</script>
+
+<div class="p-3">
+	<h1>Confirm Appointment</h1>
+	<p>Doctor Name:</p>
+	<p>Date:</p>
+	<p>Time:</p>
+	<p>Type of Operation:</p>
+	<button on:click={cancelAppointment} type="submit" class="btn btn-danger">Cancel</button>
+	<button type="submit" class="btn btn-primary">Confirm</button>
+</div>
