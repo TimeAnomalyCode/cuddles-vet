467839c5ae82d8737559e83b5a4d4b9cb19ae9b6
TimeAnomalyCode <77450287+TimeAnomalyCode@users.noreply.github.com>
2023-11-20 23:11:20 +0800

    add api route for cart

---
+++ src/routes/api/addToCart/[user_id]/[product_id]/[quantity]/+server.js
@@ -0,0 +1,7 @@
+import { json } from '@sveltejs/kit';
+
+/** @type {import('./$types').RequestHandler} */
+export async function POST({ params }) {
+    // console.log(params)
+    return json({ status: 200, message: "success" })
+};
