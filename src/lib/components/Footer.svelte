08681df55296706b87304e7b63335d314d1cb63c
tingxi01 <tingxi99@gmail.com>
2023-11-22 09:55:17 +0800

    footer added social media icons

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,9 +1,28 @@
-<footer>
-	<div class="footer__color text-center p-3">2023 Cuddles Vet Clinic</div>
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet"/>
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"/>
+<footer class="footer">
+  <div class="container text-center">
+    <a href="https://www.facebook.com/CuddlesVeterinaryHospital/"><i class="fa fa-facebook"></i></a>
+    <a href="https://www.instagram.com/cuddlesvet/"><i class="fa fa-instagram"></i></a>
+    <a href="https://my.linkedin.com/in/melissa-yung-015792167"><i class="fa fa-linkedin"></i></a>
+  </div>
+  <div class="text-center p-3">2023 Cuddles Vet Clinic</div>
 </footer>
 
 <style>
-	.footer__color {
-		background-color: #d0c4be;
+
+	.footer {
+		background: rgba(208, 196, 190, 0.4);
+		padding: 10px 0;
 	}
+	.footer a {
+		color: #736D69; 
+		font-size: 1.3vw;
+		padding: 1%;
+		transition: all .5s ease;
+	}
+	.footer a:hover {
+  		color: white;
+	}
+	
 </style>
