fb45c8b811bd529768c4c1c8f903a5b6935a1c19
TimeAnomalyCode <77450287+TimeAnomalyCode@users.noreply.github.com>
2023-11-22 09:46:20 +0800

    finish appointment history

--- src/routes/appointment-history/+page.svelte
+++ src/routes/appointment-history/+page.svelte
@@ -1,8 +1,45 @@
 <script>
+	import { goto } from '$app/navigation';
+
 	/** @type {import('./$types').PageData} */
 	export let data;
+	const appointments = data.appointments;
+	// console.log(appointments);
+	function backToProfile() {
+		goto('/profile');
+	}
 </script>
 
 <div class="p-3">
 	<h2>Appointment History</h2>
+	<div class="container">
+		{#each appointments as apt (apt.id)}
+			<div class="row" style="background-color: cornsilk">
+				<div class="col">
+					<strong>Appointment ID</strong>
+					<p>{apt.id}</p>
+				</div>
+				<div class="col">
+					<strong>Date</strong>
+					<p>
+						{new Date(apt.date).toDateString()}
+					</p>
+				</div>
+				<div class="col">
+					<strong>Start Time</strong>
+					<p>{new Date(apt.date).toTimeString()}</p>
+				</div>
+				<div class="col">
+					<strong>Doctor Name</strong>
+					<p>{apt.doctor.name}</p>
+				</div>
+				<div class="col">
+					<strong>Type of Operation</strong>
+					<p>{apt.type_of_operation}</p>
+				</div>
+			</div>
+			<br />
+		{/each}
+		<button on:click={backToProfile} class="btn btn-primary">Back</button>
+	</div>
 </div>
